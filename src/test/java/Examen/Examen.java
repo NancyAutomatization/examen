5cd72d942b4c39400d680b3ef995eefbf2d654cc
Tester Nancy <nancy.fierro.arenas@gmail.com>
2021-06-12 12:21:05 -0500

    Primer examen

---
+++ src/test/java/Examen/Examen.java
@@ -0,0 +1,47 @@
+package Examen;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Examen
+{
+    private WebDriver driver;
+    @Before
+    public void setUp ()
+    {
+        //Se coloca la dirección del driver, sea chrome, sea mozilla o edge.
+        System.setProperty("webdriver.chrome.driver","src/main/Drivers/Chrome.Drivers/chromedriver.exe");
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com/");
+
+    }
+
+    @Test
+    public void Examen () throws InterruptedException
+    {
+        WebElement inputBuscar = driver.findElement(By.xpath("//input[@name='q']"));
+        inputBuscar.click();
+        inputBuscar.clear();
+        inputBuscar.sendKeys("Alexa");
+        inputBuscar.submit();
+        Thread.sleep(2000);
+
+    }
+
+    @After
+    public void tearDown()
+    {
+        //Cierra el navegador
+        driver.quit();
+    }
+
+}
